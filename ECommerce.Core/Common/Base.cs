56fa1539a893437148cdc0a3a81fc9a6e81ee12b
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2023-05-07 03:48:35 +0300

    Add project files.

---
+++ ECommerce.Core/Common/Base.cs
@@ -0,0 +1,6 @@
+﻿namespace ECommerce.Core.Common;
+
+public class Base<TKey>
+{
+    public TKey Id { get; set; }
+}
---
+++ ECommerce.Core/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Core.Common;
+
+namespace ECommerce.Core.Entities;
+
+public class Product : Base<string>
+{
+    public string Name { get; set; }
+
+    public string MyProperty { get; set; }
+}
